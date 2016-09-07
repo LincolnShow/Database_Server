c89710682445568613f556b7b00cb39d50c12e44
Alexander Shaw <shou.aa00@gmail.com>
2016-09-07 13:32:03 +0300

    Test Script updated

--- TEST FILES/RUN_TESTS.sh
+++ TEST FILES/RUN_TESTS.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+total="RESULTS/TOTAL.result"
+> $total
 for filename in TESTS/*.txt; do
 	name=${filename##*/}
 	name=${name%.txt}
 	name="$name.result"
-	./DB_Client 12345 t $filename > RESULTS/$name
+	#./DB_Client 12345 t $filename > RESULTS/$name
+
+	resultFile="RESULTS/$name"
+	if grep -Fq failed $resultFile
+	then
+	echo "$name...BAD" >> $total
+	else
+	echo "$name...GOOD" >> $total
+	fi
 done 
